e1cbfded723bc6f8f3717718c9f5469ce3341ec9
dafyddllyr <dafyddllyr@gmail.com>
2019-05-13 09:50:36 +0100

    Updated table of contents challenges

--- chapter_6_more_about_methods/table_of_contents.rb
+++ chapter_6_more_about_methods/table_of_contents.rb
@@ -1,10 +1,10 @@
 # Here's something for you to do in order to play around more with 'center', 'ljust' and 'rjust': write a program that will display a table of contents.
 
-line_width = 50
+line_width = 70
 title = "Table of Contents".center(line_width)
-chapter_1 = "Chapter 1: Getting Started".ljust(30) + "page 1".rjust(20)
-chapter_2 = "Chapter 2: Numbers".ljust(30) + "page 9".rjust(20)
-chapter_3 = "Chapter 3: Letters".ljust(30) + "page 13".rjust(20)
+chapter_1 = "Chapter 1: Getting Started".ljust(50) + "page 1".rjust(20)
+chapter_2 = "Chapter 2: Numbers".ljust(50) + "page 9".rjust(20)
+chapter_3 = "Chapter 3: Letters".ljust(50) + "page 13".rjust(20)
 
 puts title
 puts
--- chapter_8_arrays_and_iterators/table_of_contents_revisited.rb
+++ chapter_8_arrays_and_iterators/table_of_contents_revisited.rb
@@ -1,1 +1,20 @@
-# Rewrite your table of contents program. Start the program with an array holding all the information for your table of contents (chapter names, page numbers, and so on). Then print out the information from the array in a beautifully formatted table of contents
+# Rewrite your table of contents program. Start the program with an array holding all the information for your table of contents (chapter names, page numbers, and so on). Then print out the information from the array in a beautifully formatted table of contents.
+
+title = "Table of Contents"
+
+chapter_array = [
+  ["Getting Started", 1],
+  ["Numbers", 9],
+  ["Letters", 13],
+  ["Variabes and Assignment", 29]
+]
+
+chap_num = 1
+line_width = 70
+
+puts "-- #{title} --".center(line_width)
+puts 
+chapter_array.each { |x|
+  puts "Chapter #{chap_num}: #{x[0]}".ljust(line_width/2) + "Page #{x[1]}".rjust(line_width/2)
+  chap_num += 1
+}
